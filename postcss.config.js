bd6430d89b23754c59ede743a8d8004b6984985b
hyoon <12157850+2hyoon@users.noreply.github.com>
2026-02-17 22:11:06 -0500

    updated packages with tailwinds

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
 const autoprefixer = require('autoprefixer');
 
+// module.exports = {
+//   plugins: [autoprefixer],
+// };
+
 module.exports = {
-  plugins: [autoprefixer],
+  plugins: {
+    '@tailwindcss/postcss': {}, // v4 전용 포스트프로세서
+    autoprefixer: {},
+  },
 };
